6b96ff901e7674f3259dab7fdf368f2fb16d2dbe
klarangrd <klara.ngaard@gmail.com>
2024-11-12 15:30:01 +0100

    Fetched menu data dynamically from Azure Storage Table

---
+++ IBAS-kantine2/Model/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+public class MenuItem : ITableEntity
+{
+    public string PartitionKey { get; set; } = "CanteenMenu";
+    public string RowKey { get; set; } // Day of the week
+    public string ColdDish { get; set; }
+    public string HotDish { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+}
--- IBAS-kantine2/Pages/Index.cshtml.cs
+++ IBAS-kantine2/Pages/Index.cshtml.cs
@@ -1,19 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IBAS_kantine2.Pages;
-
-public class IndexModel : PageModel
+namespace IndexCSS
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly TableClient _tableClient;
+        private readonly string connectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=ibasbikeproductionklara;AccountKey=tYjOHBSk7X19PyCg56caEKfZc0aMtSHQ4HiAOg2FFyhM/XdtWXlTGL0v9QKOBYQ2bA1Rxgf+IMxe+ASt25TUBw==;BlobEndpoint=https://ibasbikeproductionklara.blob.core.windows.net/;FileEndpoint=https://ibasbikeproductionklara.file.core.windows.net/;QueueEndpoint=https://ibasbikeproductionklara.queue.core.windows.net/;TableEndpoint=https://ibasbikeproductionklara.table.core.windows.net/";
 
-    public void OnGet()
-    {
+        public List<MenuItem> MenuItems { get; set; } = new();
 
+        public IndexModel()
+        {
+            _tableClient = new TableClient(connectionString, "WeeklyMenu"); // Ensure this matches your table name
+        }
+
+        public void OnGet()
+        {
+            try
+            {
+                // Fetch all items with PartitionKey "CanteenMenu"
+                var items = _tableClient.Query<MenuItem>(m => m.PartitionKey == "CanteenMenu");
+
+                foreach (var item in items)
+                {
+                    Console.WriteLine($"Day: {item.RowKey}, ColdDish: {item.ColdDish}, HotDish: {item.HotDish}");
+                    MenuItems.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                // For development, consider displaying the error on the page temporarily for troubleshooting
+            }
+        }
     }
 }
